d96232440f50f131021c1cc2e25ee8c9e2fb1a82
Rajeshwari Rapelli <rajeshwari.rapelli@graymatrix.com>
2018-11-11 22:51:15 +0530

    added new abstract class

---
+++ statlib/src/main/java/com/example/statlib/DisplayText.java
@@ -0,0 +1,15 @@
+package com.example.statlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public abstract class DisplayText {
+
+    public void displayText(Context context) {
+        Toast.makeText(context, "Hello World", Toast.LENGTH_SHORT).show();
+    }
+
+    public void displayText(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
